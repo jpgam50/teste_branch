e2a5d99125055e574842b0a30cb835dca2767f79
joaogambaro <joaopaulogambaro@gmail.com>
2022-09-22 18:43:26 -0300

    correção na automação

--- scripts/envio_pacote.sh
+++ scripts/envio_pacote.sh
@@ -3,19 +3,9 @@
 #a construção do pacote e envia este arquivo paro o servidor do gemfury
 
 
-
-PATH_SCRIPT=$(realpath $"2_model_package")  #diretório do script
-#PATH_ROOT=${PATH_SCRIPT%*envipacote.sh*} #diretório raiz do projeto
-#PATH_MODEL_PKC=${PATH_ROOT}"/2_model_package" #diretório das pastas do projeto
-
-
-
-#deretório do arquivo .gz
-FOLDER=${PATH_SCRIPT}"/dist"
+PATH_PROJETO=$(realpath $"2_model_package")  #diretório do projeto do pacote
+FOLDER=${PATH_PROJETO}"/dist"                #diretório do arquivo .gz
 ARQUIVO=""
-
-echo PATH_SCRIPT $PATH_SCRIPT
-echo FOLDER $FOLDER
 
 
 #verifica se o arquivo existe e se é um diretório
@@ -27,19 +17,14 @@ if [ -d $FOLDER ]; then
     #identifica o arquivo .gz
     if [[ $file == *.gz ]]; then
       ARQUIVO=$file
-      echo "Arquivo encontrado, nome do arquivo:"
-      echo "  $ARQUIVO"
+      echo "Arquivo encontrado, nome do arquivo: '$ARQUIVO' "
     fi
-
   done
 
-
   #envia o arquivo para o servidor
-  curl -F package=@$"${FOLDER}/$ARQUIVO" "$GEMFURRY_URL"
-  echo Endereço do servidor:  ${FOLDER}/$ARQUIVO
+  curl -F package=@$FOLDER/$ARQUIVO "$GEMFURRY_URL"
+  echo Código de retorno da função que envia o pacote: $?
 
 else
-  echo ""
-  echo "--- erro:pacote não enviado ---"
-  echo "O diretório $FOLDER não exite"
+  echo "Erro:pacote não enviado, o diretório '$FOLDER' não existe"
 fi
