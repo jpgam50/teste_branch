d05b05ff954192f030c74fdc467ea226be2e15f9
joaogambaro <joaopaulogambaro@gmail.com>
2022-10-01 16:04:18 -0300

    coment

--- 2_model_package/setup.py
+++ 2_model_package/setup.py
@@ -18,7 +18,7 @@ setup(
     #packages=find_packages(exclude=("tests",)),
     packages=find_packages(),
     install_requires=[
-      'feature-engine==1.4.1',
+      'feature-engine==1.4.1',  #OBS: quando eu removi esta funcao deu erro no circleci, entender o pq
       'joblib==1.1.0',
       'numpy==1.23.2',
       'pandas==1.4.3',
